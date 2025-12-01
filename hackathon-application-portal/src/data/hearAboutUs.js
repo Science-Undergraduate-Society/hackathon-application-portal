c52808c10c162a58cb3f7cec698ab0c10035e7d9
Eojin Lee <142281569+elee131@users.noreply.github.com>
2025-12-01 03:39:15 -0800

    add more data for dropdowns

---
+++ hackathon-application-portal/src/data/hearAboutUs.js
@@ -0,0 +1,11 @@
+export const howDidYouHear = [
+  { value: "discord", label: "Discord Server" },
+  { value: "linkedin", label: "LinkedIn" },
+  { value: "instagram", label: "Instagram" },
+  { value: "word-of-mouth", label: "Word of Mouth" },
+  { value: "email", label: "Email Newsletter" },
+  { value: "coding-club", label: "Coding Club / Student Group" },
+  { value: "previous-event", label: "Previous Event" },
+  { value: "website", label: "Hackathon Website" },
+  { value: "other", label: "Other: Please enter custom input." }
+];
--- hackathon-application-portal/src/data/pronouns.js
+++ hackathon-application-portal/src/data/pronouns.js
@@ -2,5 +2,5 @@ export const pronouns = [
   { value: "she-her-hers", label: "she/her/hers" },
   { value: "he-him-his", label: "he/him/his" },
   { value: "they-them-theirs", label: "they/them/theirs" },
-  { value: "other", label: "other" }
+  { value: "other", label: "Other: Please type your pronoun." }
 ];
