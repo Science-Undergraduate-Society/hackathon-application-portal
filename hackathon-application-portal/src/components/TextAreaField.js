f48a33624ee69fed0d66ab95c0ef311e31683534
Eojin Lee <142281569+elee131@users.noreply.github.com>
2025-12-01 08:23:23 -0800

    change appearance of general questions tab

--- hackathon-application-portal/src/components/TextAreaField.js
+++ hackathon-application-portal/src/components/TextAreaField.js
@@ -1,19 +1,20 @@
 'use client';
+import './TextAreaField.css'
 
 export default function TextAreaField({ label, value, required = false, maxLength, onChange }) {
   return (
     <div className="field">
       <label>
         {label}{required && ' *'}
+          </label>
         <textarea
           value={value}
           required={required}
           maxLength={maxLength}
-          rows={6}
           onChange={onChange}
         />
         <div>{value.length}/{maxLength}</div>
-      </label>
+    
     </div>
   );
 }
