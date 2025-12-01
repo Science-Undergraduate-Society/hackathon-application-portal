e6e59f0ca06341fdab042e6c98e13a37fb12246e
Eojin Lee <142281569+elee131@users.noreply.github.com>
2025-12-01 06:05:36 -0800

    add new questions

--- hackathon-application-portal/src/app/application/general-questions/page.js
+++ hackathon-application-portal/src/app/application/general-questions/page.js
@@ -3,49 +3,73 @@
 import useProfileForm from '../../../hooks/useProfileForm';
 import TextAreaField from '../../../components/TextAreaField';
 import './general-questions.css';
+import { ConfirmBtn, ForwardBtn } from '@/components/CommonUI';
 
 export default function ApplicationQuestionsPage() {
   const initialState = {
     question1: '',
     question2: '',
-    question3: ''
+    question3: '',
+    question4: '',
+    question5: ''
   };
+
   const { form, handleChange, loading, handleNext, handleBack } = useProfileForm(
     initialState,
-    '/application/review',       // ← now points to the review page
-    '/application/hacker-extra'  // ← back to page 2
+    '/application/review',
+    '/application/hacker-extra'
   );
 
   if (loading) return <div>Loading...</div>;
 
   return (
     <main>
-      <h1>Additional Questions</h1>
+      <h1>Application Questions</h1>
+
       <TextAreaField
-        label="Why do you want to attend this hackathon?"
+        className="sprite-dolphin"
+        label="What do you hope to learn from attending this hackathon? Why?"
         value={form.question1}
         required
         maxLength={300}
         onChange={handleChange('question1')}
       />
+
       <TextAreaField
-        label="If you could build anything — no constraints, no limits — what would you build?"
+        label="What is a recent challenge you faced, and how did you approach it?"
         value={form.question2}
         required
         maxLength={300}
         onChange={handleChange('question2')}
       />
+
       <TextAreaField
-        label="Anything else you’d like us to know?"
+        className="sprite-otter-turtle"
+        label="Describe the kind of role you play on a team and how it's valuable/beneficial to the team."
         value={form.question3}
-        maxLength={150}
+        required
+        maxLength={300}
         onChange={handleChange('question3')}
       />
 
+      <TextAreaField
+        label="What is your favourite body of water? Why? (e.g. pond/ocean/bathtub)"
+        value={form.question4}
+        maxLength={300}
+        onChange={handleChange('question4')}
+      />
+
+      <TextAreaField
+        className="sprite-whale"
+        label="Suggest a song and we'll add it to our hackathon playlist!"
+        value={form.question5}
+        maxLength={150}
+        onChange={handleChange('question5')}
+      />
+
       <div className="buttons">
-        <button type="button" onClick={handleBack}>Back</button>
-        <button type="button" onClick={handleNext}>Next</button>
-      </div>
+
+   <ConfirmBtn onClickFn={handleNext} dimension={"lg"}></ConfirmBtn>      </div>
     </main>
   );
 }
