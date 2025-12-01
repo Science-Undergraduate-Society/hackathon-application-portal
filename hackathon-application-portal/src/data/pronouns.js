3c10d5593c1b8220dd8803143c1fca64c7d0efb2
Eojin Lee <142281569+elee131@users.noreply.github.com>
2025-12-01 03:19:45 -0800

    add data for dropdowns

---
+++ hackathon-application-portal/src/data/pronouns.js
@@ -0,0 +1,6 @@
+export const pronouns = [
+  { value: "she-her-hers", label: "she/her/hers" },
+  { value: "he-him-his", label: "he/him/his" },
+  { value: "they-them-theirs", label: "they/them/theirs" },
+  { value: "other", label: "other" }
+];
