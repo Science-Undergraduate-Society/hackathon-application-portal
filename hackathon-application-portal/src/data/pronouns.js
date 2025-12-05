4aa0a494c4f0471c7f78453ca491acf992458ba9
alan-w-u <alanwu0004@gmail.com>
2025-12-05 01:06:15 -0800

    general UI changes

--- hackathon-application-portal/src/data/pronouns.js
+++ hackathon-application-portal/src/data/pronouns.js
@@ -2,5 +2,5 @@ export const pronouns = [
   { value: "she-her-hers", label: "she/her/hers" },
   { value: "he-him-his", label: "he/him/his" },
   { value: "they-them-theirs", label: "they/them/theirs" },
-  { value: "other", label: "Other: Please type your pronoun." }
+  { value: "other", label: "other (please type your pronouns)" }
 ];
